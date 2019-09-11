19097df3a2e5e1f5fc099a6ae3810c48c89b9523
fox <38516517+foxliang@users.noreply.github.com>
2019-09-11 18:32:37 +0800

    Create test2.go

---
+++ Go/my_go/进度条/test2.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+    "time"
+)
+
+func main() {
+	ti := time.Now()
+	fmt.Println("开始 : ", ti.Format("2006-01-02 15:04:05"))
+	for i := 0; i<50; i++{
+		fmt.Printf("%d%% [%s]\r",i,getS(i,"#") + getS(50-i," "))
+		time.Sleep(time.Duration(100) * time.Millisecond)  //延迟输出
+	}
+	elapsed := time.Since(ti)
+	fmt.Println("\n结束，总共耗时: ", elapsed)
+}
+
+
+func getS(n int,char string) (s string) {
+    for i:=1;i<=n;i++{
+        s+=char
+    }
+    return
+}
+
+
