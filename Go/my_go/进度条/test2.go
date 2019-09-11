5c860145c7f352c7ef9a4805a27f3a10aea7c6f1
fox <38516517+foxliang@users.noreply.github.com>
2019-09-11 18:37:26 +0800

    Update test2.go

--- Go/my_go/进度条/test2.go
+++ Go/my_go/进度条/test2.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	ti := time.Now()
 	fmt.Println("开始 : ", ti.Format("2006-01-02 15:04:05"))
-	for i := 0; i<50; i++{
-		fmt.Printf("%d%% [%s]\r",i,getS(i,"#") + getS(50-i," "))
+	for i := 1; i<=100; i++{
+		fmt.Printf("%d%% [%s]\r",i,getS(i,"#") + getS(100-i," "))
 		time.Sleep(time.Duration(100) * time.Millisecond)  //延迟输出
 	}
 	elapsed := time.Since(ti)
