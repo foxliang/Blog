54355f2415db58bae2ddbc431fcc081b3ab7919b
fox <38516517+foxliang@users.noreply.github.com>
2020-10-22 10:37:16 +0800

    Create 1. 两数之和(2).go

---
+++ Go/my_go/leetcode/1. 两数之和(2).go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/*
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+哈希表解法
+*/
+
+func main() {
+	var (
+		nums   = []int{2, 7, 11, 15}
+		target = 9
+	)
+	res := twoSum(nums, target)
+	fmt.Println("res", res)
+}
+
+func twoSum(nums []int, target int) []int {
+	hashTable := map[int]int{}
+	for i, x := range nums {
+		if p, ok := hashTable[target-x]; ok {
+			return []int{p, i}
+		}
+		hashTable[x] = i
+	}
+	return nil
+}
+
+//go run main.go
+//res [0 1]
