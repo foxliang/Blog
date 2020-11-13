ab1278dcb730a31dce7451c26e0cd1418655d51a
fox <38516517+foxliang@users.noreply.github.com>
2020-11-13 16:08:03 +0800

    Update demo.go

--- Go/my_go/channel/demo.go
+++ Go/my_go/channel/demo.go
@@ -37,3 +37,56 @@ func main() {
 }
 
 ```
+
+
+2.
+```
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// 理念：不要通过共享内存来通信；通过通信来共享内存
+
+/**
+channel 例子二： channel数组
+*/
+
+func worker2(id int, c chan int) {
+	for {
+		// 从 channel 里面获取数据，赋值给 n
+		n := <-c
+		fmt.Println("接收到的数据： ", n)
+
+		// 发现输出的顺序不一样，这是因为，运行的时候是乱序的，谁先执行是随机的
+
+	}
+}
+
+func main() {
+	// var c chan int // 此时 c == nil
+	var channels [10]chan int // channel 数组
+
+	for i := 0; i < 10; i++ {
+		// 创建一个 chan
+		channels[i] = make(chan int)
+		// 接收 channel 的数据，只能在另一个 协程(goruotime)里面才能接收到
+		go worker2(i, channels[i])
+	}
+
+	// 往  channel 发送数据
+	for i := 0; i < 10; i++ {
+		channels[i] <- i
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- i
+	}
+
+	// 睡眠一下 把两个循环中的值都打印出来
+	time.Sleep(time.Millisecond)
+
+}
+```
