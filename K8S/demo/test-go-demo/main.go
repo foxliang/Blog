10a7cc40e2b51f351d2790fb999f9a196d393085
fox <38516517+foxliang@users.noreply.github.com>
2021-04-15 10:25:01 +0800

    Update main.go

--- K8S/demo/test-go-demo/main.go
+++ K8S/demo/test-go-demo/main.go
@@ -1,14 +1,24 @@
 package main
- 
+
 import (
+	"flag"
+
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
- 
+
+var version = flag.String("v", "v1", "v1")
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "PONG")
+
+	router.GET("", func(c *gin.Context) {
+		flag.Parse()
+		hostname, _ := os.Hostname()
+
+		c.String(http.StatusOK, "This is version:%s running in pod %s",*version,hostname)
 	})
+
+
 	router.Run(":8080")
 }
