1242dc8ac8ee99df770fdb7f8c0ee37f28d14594
fox <38516517+foxliang@users.noreply.github.com>
2021-04-27 18:33:26 +0800

    Create mysql.sql

---
+++ K8S/demo/go-mysql/mysql/mysql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `go` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL DEFAULT '' COMMENT 'name',
+  `create_time` int unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` int unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+  `up_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='test';
