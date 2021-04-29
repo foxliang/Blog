22a7fca939f45697bfa332c4de976ef99614cbf5
fox <38516517+foxliang@users.noreply.github.com>
2021-04-29 16:47:46 +0800

    Update mysql.go

--- K8S/demo/go-mysql/mysql/mysql.go
+++ K8S/demo/go-mysql/mysql/mysql.go
@@ -2,11 +2,24 @@ package mysql
 
 import (
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"sync"
 	"xorm.io/xorm"
-
-	_ "github.com/go-sql-driver/mysql"
 )
+
+var (
+	currentConfig Config
+)
+
+// GetCfg get current config
+func GetCfg() *Config {
+	return &currentConfig
+}
+
+// SetCfg set current config
+func SetCfg(cfg *Config) {
+	currentConfig = *cfg
+}
 
 var (
 	_dbOnce sync.Once
@@ -23,14 +36,17 @@ type Config struct {
 
 // GetDB get db instance from config
 func GetDB() *xorm.Engine {
+	//fmt.Println("GetCfg().Addr", GetCfg().Addr)
 	_dbOnce.Do(func() {
-		engine, err := xorm.NewEngine("mysql", "root:123456@tcp(192.168.79.2:31306)/test")
+		//Addr = "root:123456@tcp(192.168.79.2:31306)/test"
+		//engine, err := xorm.NewEngine("mysql", "root:123456@tcp(192.168.79.2:31306)/test")
+		engine, err := xorm.NewEngine("mysql", GetCfg().Addr)
 		if err != nil {
 			fmt.Println("mysql connection err", err)
 		}
 
-		engine.SetMaxOpenConns(8)
-		engine.SetMaxIdleConns(2)
+		engine.SetMaxOpenConns(GetCfg().MaxOpenConns)
+		engine.SetMaxIdleConns(GetCfg().MaxIdleConns)
 		//engine.SetLogger(newDbLogger())
 		//engine.ShowSQL(true)
 
